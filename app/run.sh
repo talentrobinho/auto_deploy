cdea595e4ba37883f41f18d75234bc8064d91029
beaEpoch <du.hrbeu@gmail.com>
2017-02-10 16:40:56 +0800

    עÊÍhello

--- app/run.sh
+++ app/run.sh
@@ -4,3 +4,5 @@ nohup mongod &
 nohup redis-server /etc/redis/redis.conf &
 gunicorn hemera:app -k gevent -b 0.0.0.0:5000
 #nohup gunicorn -b 0.0.0.0:80 -k eventlet -w 40 -t 99999 run:app &
+
+#hello world
